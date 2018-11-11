5775ab2b7309b937b3477d238e19464f2a481740
jxxbd91 <jxxbd91@163.com>
2018-11-11 11:33:39 +0800

    修复小bug

--- cloudfunctions/getIndexList/index.js
+++ cloudfunctions/getIndexList/index.js
@@ -6,6 +6,7 @@ const db = cloud.database()
 const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
+  let { OPENID, APPID, UNIONID } = cloud.getWXContext() 
   const aDay = 24 * 60 * 60 * 1000
   const now = new Date()
   const todayS = new Date(now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()).getTime() - 1
@@ -16,16 +17,21 @@ exports.main = async (event, context) => {
   const dayE = dayS + 7 * aDay
   return Promise.all([
     await db.collection('todos').where({
+      _openid: OPENID,
       completeDate: _.gte(todayS).and(_.lt(tomoS)),
       done: false
     }).count(),
     await db.collection('todos').where({
+      _openid: OPENID,
       done: true
     }).count(),
     await db.collection('todos').where({
+      _openid: OPENID,
       completeDate: _.gte(dayS).and(_.lt(dayE)),
       done: false
     }).count(),
-    await db.collection('todos').count()
+    await db.collection('todos').where({
+      _openid: OPENID
+    }).count()
   ])
 }
--- cloudfunctions/getProjectList/index.js
+++ cloudfunctions/getProjectList/index.js
@@ -5,11 +5,15 @@ cloud.init()
 const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let {data = []} = await db.collection('projects').get()
+  let { OPENID, APPID, UNIONID } = cloud.getWXContext() 
+  let { data = [] } = await db.collection('projects').where({
+    _openid: OPENID
+  }).get()
   let arr = []
   data.forEach(item => {
     let {_id} = item
     arr.push(db.collection('todos').where({
+      _openid: OPENID,
       projectId: _id
     }).get())
   })
